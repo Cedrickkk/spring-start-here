ae481454d755c83f7eb129eb9e73f384655a9e23
hulaanmo <inhog.cedrick@gmail.com>
2025-09-23 15:01:56 +0800

    finish chapter 8 exercises

---
+++ sq-ch8-ex6/src/main/java/org/sring/framework/sqch8ex6/SqCh8Ex6Application.java
@@ -0,0 +1,13 @@
+package org.sring.framework.sqch8ex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh8Ex6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqCh8Ex6Application.class, args);
+    }
+
+}
---
+++ sq-ch8-ex6/src/main/java/org/sring/framework/sqch8ex6/models/Product.java
@@ -0,0 +1,19 @@
+package org.sring.framework.sqch8ex6.models;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
